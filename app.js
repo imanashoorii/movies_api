a8f5dc163e1a5ebd6d36bd597e52a151e33ae9bf
imanashoorii <imanashoorii.77@gmail.com>
2021-03-11 14:53:41 +0330

    some change on database conn

--- app.js
+++ app.js
@@ -3,21 +3,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const app = express();
 
-// async() => {
-//     try {
-//         await mongoose.connect('mongodb://root:root@192.168.56.3:27017/test', {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//             useFindAndModify: false,
-//             useCreateIndex: true
-//         });
-//     } catch(err) {
-//         throw err
-//     }
-// }
-
 mongoose.connect("mongodb://root:root@192.168.56.3:27017", {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 },
 err => {
     if (!err) {
